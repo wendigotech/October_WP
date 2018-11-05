ab4e38a0e1f0ec7169a77c35755abc46dec82da1
MB <wendigotech@gmail.com>
2018-11-05 19:28:01 +0100

    20

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
                     <nav class="navbar navbar-expand-lg fixed-top"> 
                         <div class="container"> 
                             <?php if ( ! has_custom_logo() ) : ?>
-                                <div class="grid-logo" style="grid-area:1 / 4 / 2 / 5;"> 
+                                <div class="grid-logo d-lg-none d-xl-none" style="grid-area:1 / 4 / 2 / 5;"> 
                                     <a rel="home" class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> 
                                 </div>                                 
                             <?php else : ?>
