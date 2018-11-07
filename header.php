690420f584f8519fc1e8f0454d281b49b48c8deb
MB <wendigotech@gmail.com>
2018-11-07 22:40:03 +0100

    header grid

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
                 <div> 
                     <div class="jumbotron">
                         <div class="container">
-                            <div class="grid">
+                            <div class="grid" style="display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: 1fr; grid-gap: 10px;">
                                 <figure class="effect-ruby">
                                     <a href="#" style="position: relative; overflow: hidden;">
                                         <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
@@ -56,11 +56,30 @@
                                     </figcaption>                                     
                                 </figure>
                                 <figure class="effect-ruby">
-                                    <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img14">
+                                    <a href="#" style="position: relative; overflow: hidden;">
+                                        <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
+                                    </a>
                                     <figcaption>
                                         <h2><?php _e( 'Glowing', 'october' ); ?> <span><?php _e( 'Ruby', 'october' ); ?></span></h2>
-                                        <p><?php _e( 'Ruby did not need any help. Everybody knew that.', 'october' ); ?></p>
-                                        <a href="#"><?php _e( 'View more', 'october' ); ?></a>
+                                        <p><a class="btn btn-light" href="#"><?php _e( 'Visit Casino', 'october' ); ?></a><a class="btn btn-light" href="#" data-toggle="tooltip" data-placement="bottom" title="abc"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a></p>
+                                    </figcaption>                                     
+                                </figure>
+                                <figure class="effect-ruby">
+                                    <a href="#" style="position: relative; overflow: hidden;">
+                                        <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
+                                    </a>
+                                    <figcaption>
+                                        <h2><?php _e( 'Glowing', 'october' ); ?> <span><?php _e( 'Ruby', 'october' ); ?></span></h2>
+                                        <p><a class="btn btn-light" href="#"><?php _e( 'Visit Casino', 'october' ); ?></a><a class="btn btn-light" href="#" data-toggle="tooltip" data-placement="bottom" title="abc"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a></p>
+                                    </figcaption>                                     
+                                </figure>
+                                <figure class="effect-ruby">
+                                    <a href="#" style="position: relative; overflow: hidden;">
+                                        <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
+                                    </a>
+                                    <figcaption>
+                                        <h2><?php _e( 'Glowing', 'october' ); ?> <span><?php _e( 'Ruby', 'october' ); ?></span></h2>
+                                        <p><a class="btn btn-light" href="#"><?php _e( 'Visit Casino', 'october' ); ?></a><a class="btn btn-light" href="#" data-toggle="tooltip" data-placement="bottom" title="abc"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a></p>
                                     </figcaption>                                     
                                 </figure>
                             </div>
