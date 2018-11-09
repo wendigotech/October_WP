c84db9be6011088119e69dcdeb481c82974e0770
MB <wendigotech@gmail.com>
2018-11-09 23:38:06 +0100

    header content carousel with buttons

--- index.php
+++ index.php
@@ -32,17 +32,17 @@ get_header(); ?>
             <div class="container py-3 col-lg-6 col-md-6">
                 <div class="card card-transparent">
                     <div class="row ">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-6">
                             <figure class="snip0077 blue">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45"/>
-                                <figcaption>
-                                    <p><?php _e( 'I propose we leave math to the machines and go play outside.', 'october' ); ?></p>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45" class="snip0077"/>
+                                <figcaption class="snip0077">
+                                    <p><?php _e( 'Read more', 'october' ); ?></p>
                                 </figcaption>
                             </figure>
                         </div>
-                        <div class="px-3 col-md-6">
+                        <div class="px-3 col-md-6 col-sm-6 col-6">
                             <div class="card-block px-3">
-                                <h4 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h4>
+                                <h6 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h6>
                                 <p class="card-text"><?php _e( 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore m', 'october' ); ?></p>
                             </div>
                         </div>
@@ -52,17 +52,17 @@ get_header(); ?>
             <div class="container py-3 col-lg-6 col-md-6">
                 <div class="card card-transparent">
                     <div class="row ">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-6">
                             <figure class="snip0077 blue">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45"/>
-                                <figcaption>
-                                    <p><?php _e( 'I propose we leave math to the machines and go play outside.', 'october' ); ?></p>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45" class="snip0077"/>
+                                <figcaption class="snip0077">
+                                    <p><?php _e( 'Read more', 'october' ); ?></p>
                                 </figcaption>
                             </figure>
                         </div>
-                        <div class="px-3 col-md-6">
+                        <div class="px-3 col-md-6 col-sm-6 col-6">
                             <div class="card-block px-3">
-                                <h4 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h4>
+                                <h6 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h6>
                                 <p class="card-text"><?php _e( 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore m', 'october' ); ?></p>
                             </div>
                         </div>
@@ -72,17 +72,17 @@ get_header(); ?>
             <div class="container py-3 col-lg-6 col-md-6">
                 <div class="card card-transparent">
                     <div class="row ">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-6">
                             <figure class="snip0077 blue">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45"/>
-                                <figcaption>
-                                    <p><?php _e( 'I propose we leave math to the machines and go play outside.', 'october' ); ?></p>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample45.jpg" alt="sampl45" class="snip0077"/>
+                                <figcaption class="snip0077">
+                                    <p><?php _e( 'Read more', 'october' ); ?></p>
                                 </figcaption>
                             </figure>
                         </div>
-                        <div class="px-3 col-md-6">
+                        <div class="px-3 col-md-6 col-sm-6 col-6">
                             <div class="card-block px-3">
-                                <h4 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h4>
+                                <h6 class="card-title text-center"><?php _e( 'Lorem ipsum dolor sit amet', 'october' ); ?></h6>
                                 <p class="card-text"><?php _e( 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore m', 'october' ); ?></p>
                             </div>
                         </div>
@@ -185,35 +185,19 @@ get_header(); ?>
                 <h6 class="text-center"><?php _e( 'Column title', 'october' ); ?></h6> 
             </div>
         </div>                         
-        <div class="row" style="display:grid;grid-template-columns:1fr 1fr 1fr 1fr 2fr;grid-template-rows:100px 50px 150px 150px;grid-gap:5px;">
-            <div id="carousel1" class="carousel slide" data-ride="carousel" style="grid-area:1 / 1 / 6 / 5;"> 
-                <div class="carousel-inner"> 
-                    <div class="carousel-item active"> 
-                        <img class="d-block w-100" src="http://pinegrow.com/placeholders/img10.jpg" alt="First slide"> 
-                    </div>                                     
-                    <div class="carousel-item"> 
-                        <img class="d-block w-100" src="http://pinegrow.com/placeholders/img13.jpg" alt="Second slide"> 
-                        <div class="carousel-caption d-none d-md-block"> 
-                            <h3><?php _e( 'Slide label 2', 'october' ); ?></h3> 
-                            <p><?php _e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'october' ); ?></p> 
-                        </div>                                         
-                    </div>                                     
-                    <div class="carousel-item"> 
-                        <img class="d-block w-100" src="http://pinegrow.com/placeholders/img14.jpg" alt="Third slide"> 
-                    </div>                                     
-                </div>
-                <ol class="carousel-indicators justify-content-between "> 
-                    <button class="btn btn-primary active" data-target="#carousel1" aria-expanded="false" aria-controls="collapseExample" data-slide-to="0" type="button">
-                        <?php _e( 'Button', 'october' ); ?>
-                    </button>
-                    <button class="btn btn-primary" data-target="#carousel1" aria-expanded="false" aria-controls="collapseExample" data-slide-to="1" type="button">
-                        <?php _e( 'Button', 'october' ); ?>
-                    </button>
-                    <button class="btn btn-primary" data-target="#carousel1" aria-expanded="false" aria-controls="collapseExample" data-slide-to="2" type="button">
-                        <?php _e( 'Button', 'october' ); ?>
-                    </button>                                     
-                </ol>                                 
-            </div>
+        <div class="row no-gutters" style="display:grid;grid-template-columns:1fr 1fr 1fr 1fr 2fr;grid-template-rows:100px 50px 150px 150px 50px;grid-gap:5px;">
+            <figure class="effect-ruby" style="grid-area:1 / 1 / 5 / 5;">
+                <a href="#" style="position: relative; overflow: hidden;">
+                    <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
+                </a>
+                <figcaption>
+                    <h2><?php _e( 'Glowing', 'october' ); ?> </h2>
+                    <div>
+                        <a class="btn btn-light btn-sm btn-visit" href="#"><?php _e( 'Visit Casino', 'october' ); ?></a>
+                        <a class="btn btn-light btn-sm" href="#" data-toggle="tooltip" data-placement="bottom" title="abc"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
+                    </div>
+                </figcaption>                                 
+            </figure>
             <figure class="effect-ruby" style="grid-area:1 / 5 / 3 / 6;">
                 <a href="#" style="position: relative; overflow: hidden;">
                     <img src="https://images.unsplash.com/photo-1494119023197-238489d8dce3?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=d960e07ad7b81180215edea3a1071268" alt="img13">
@@ -250,6 +234,18 @@ get_header(); ?>
                     </div>
                 </figcaption>                                 
             </figure>
+        </div>
+    </div>
+    <div class="container mt-5 mb-5" id="content" tabindex="-1"> 
+        <div class="row"> 
+            <div class="col-md-12">
+                <h3 class="text-center"><?php _e( 'Column title', 'october' ); ?></h3> 
+            </div>
+        </div>                         
+        <div class="row">
+            <div class="container py-3 col-lg-10 col-md-10">
+                <p><?php _e( 'Paragraph', 'october' ); ?></p>
+            </div>
         </div>
     </div>                     
 </div>                                 
