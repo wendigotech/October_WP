833556016671af69fd5ab25a431ff13b2d5dc87c
MB <wendigotech@gmail.com>
2018-11-18 19:13:40 +0100

    jumbotron in main content

--- index.php
+++ index.php
@@ -1,6 +1,146 @@
 <?php
 get_header(); ?>
 
+<div> 
+    <div class="jumbotron">
+        <div class="container mt-5">
+            <div id="carousel1" class="carousel slide" data-ride="carousel" data-pause="hover"> 
+                <div class="carousel-inner"> 
+                    <div class="carousel-item active">
+                        <?php
+                            $best_bonus_args = array(
+                                'category_name' => 'casino',
+                                'tag' => 'best-bonus',
+                                'order' => 'DESC'
+                            )
+                        ?>
+                        <?php $best_bonus = new WP_Query( $best_bonus_args ); ?>
+                        <?php if ( $best_bonus->have_posts() ) : ?>
+                            <div <?php post_class( 'grid' ); ?> id="post-<?php the_ID(); ?>">
+                                <?php $best_bonus_item_number = 0; ?>
+                                <?php while ( $best_bonus->have_posts() && $best_bonus_item_number++ < 4 ) : $best_bonus->the_post(); ?>
+                                    <figure class="effect-ruby effect-ruby-1">
+                                        <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="position: relative; overflow: hidden;"> <?php the_post_thumbnail( 'normal' ); ?> </a>
+                                        <figcaption>
+                                            <h2><?php the_excerpt( ); ?></h2>
+                                            <div>
+                                                <a class="btn btn-light btn-visit" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
+                                                <a class="btn btn-light btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
+                                            </div>
+                                        </figcaption>                                                         
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            </div>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
+                        <?php endif; ?> 
+                    </div>
+                    <div class="carousel-item">
+                        <?php
+                            $best_casino_args = array(
+                                'category_name' => 'casino',
+                                'tag' => 'best-casino',
+                                'order' => 'DESC'
+                            )
+                        ?>
+                        <?php $best_casino = new WP_Query( $best_casino_args ); ?>
+                        <?php if ( $best_casino->have_posts() ) : ?>
+                            <div <?php post_class( 'grid' ); ?> id="post-<?php the_ID(); ?>">
+                                <?php $best_casino_item_number = 0; ?>
+                                <?php while ( $best_casino->have_posts() && $best_casino_item_number++ < 4 ) : $best_casino->the_post(); ?>
+                                    <figure class="effect-ruby effect-ruby-2">
+                                        <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="position: relative; overflow: hidden;"> <?php the_post_thumbnail( 'normal' ); ?> </a> 
+                                        <figcaption>
+                                            <h2><?php the_excerpt( ); ?></h2>
+                                            <div>
+                                                <a class="btn btn-light btn-visit" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
+                                                <a class="btn btn-light btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
+                                            </div>
+                                        </figcaption>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            </div>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
+                        <?php endif; ?> 
+                    </div>
+                    <div class="carousel-item">
+                        <?php
+                            $new_casino_args = array(
+                                'category_name' => 'casino',
+                                'tag' => 'new-casino',
+                                'order' => 'DESC'
+                            )
+                        ?>
+                        <?php $new_casino = new WP_Query( $new_casino_args ); ?>
+                        <?php if ( $new_casino->have_posts() ) : ?>
+                            <div <?php post_class( 'grid' ); ?> id="post-<?php the_ID(); ?>">
+                                <?php $new_casino_item_number = 0; ?>
+                                <?php while ( $new_casino->have_posts() && $new_casino_item_number++ < 4 ) : $new_casino->the_post(); ?>
+                                    <figure class="effect-ruby effect-ruby-1">
+                                        <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="position: relative; overflow: hidden;"> <?php the_post_thumbnail( 'normal' ); ?> </a> 
+                                        <figcaption>
+                                            <h2><?php the_excerpt( ); ?></h2>
+                                            <div>
+                                                <a class="btn btn-light btn-visit" href="<?php echo get_post_meta( get_the_ID(), 'Link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
+                                                <a class="btn btn-light btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'Link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 'T&C\'s', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
+                                            </div>
+                                        </figcaption>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            </div>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
+                        <?php endif; ?> 
+                    </div>
+                    <div class="carousel-item">
+                        <?php
+                            $most_visited_args = array(
+                                'category_name' => 'casino',
+                                'tag' => 'most-visited',
+                                'order' => 'DESC'
+                            )
+                        ?>
+                        <?php $most_visited = new WP_Query( $most_visited_args ); ?>
+                        <?php if ( $most_visited->have_posts() ) : ?>
+                            <div <?php post_class( 'grid' ); ?> id="post-<?php the_ID(); ?>">
+                                <?php $most_visited_item_number = 0; ?>
+                                <?php while ( $most_visited->have_posts() && $most_visited_item_number++ < 4 ) : $most_visited->the_post(); ?>
+                                    <figure class="effect-ruby effect-ruby-1">
+                                        <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="position: relative; overflow: hidden;"> <?php the_post_thumbnail( 'thumbnail' ); ?> </a> 
+                                        <figcaption>
+                                            <h2><?php the_excerpt( ); ?></h2>
+                                            <div>
+                                                <a class="btn btn-light btn-visit" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
+                                                <a class="btn btn-light btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
+                                            </div>
+                                        </figcaption>
+                                    </figure>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            </div>
+                        <?php else : ?>
+                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
+                        <?php endif; ?> 
+                    </div>                                     
+                </div>                                 
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 carousel-indicators d-flex flex-row align-items-center justify-content-around mt-5" style="margin-left: 1px; margin-right: 1px; margin-top: 23px;"> 
+                        <a class="btn active" data-target="#carousel1" href="#" data-slide-to="0" data-toggle="button"><?php _e( 'Best bonus', 'october' ); ?></a>
+                        <a data-target="#carousel1" href="#" data-slide-to="1" class="btn"><?php _e( 'Best casino', 'october' ); ?></a>
+                        <a data-target="#carousel1" href="#" data-slide-to="2" class="btn"><?php _e( 'New casino', 'october' ); ?></a>
+                        <a data-target="#carousel1" href="#" data-slide-to="3" class="btn"><?php _e( 'Most visited', 'october' ); ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>                         
+    </div>                     
+</div>
 <div class="wrapper" id="index-wrapper"> 
     <div class="mt-5 mb-5 container" id="content" tabindex="-1"> 
         <div class="row"> 
