baa738aa71978438ceb99595212fc89bf02cc3d0
MB <wendigotech@gmail.com>
2018-11-21 14:40:00 +0100

    8

--- page-blog.php
+++ page-blog.php
@@ -28,6 +28,11 @@ get_header(); ?>
                                                 <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                                                 
                                                 <li data-target="#carousel1" data-slide-to="1"></li>                                                                 
                                                 <li data-target="#carousel1" data-slide-to="2"></li>                                                                 
+                                            </ol>
+                                            <ol class="carousel-indicators"> 
+                                                <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                                                 
+                                                <li data-target="#carousel1" data-slide-to="1"></li>                                                                 
+                                                <li data-target="#carousel1" data-slide-to="2"></li>                                                                 
                                             </ol>                                                             
                                         </div>                                                         
                                     </div>
