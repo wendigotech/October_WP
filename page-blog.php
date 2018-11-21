80440a16c74fd4c7d2c40835f508c31c67cc19cc
MB <wendigotech@gmail.com>
2018-11-21 01:46:50 +0100

    3

--- page-blog.php
+++ page-blog.php
@@ -33,39 +33,53 @@ get_header(); ?>
                                             </ol>                                                                                                                                                                                       
                                         </div>                                                         
                                     </div>
-                                    <div class="entry-content"> 
-                                        <p class="mt-5"><?php the_content(); ?></p> 
-                                    </div>
-                                    <?php
-                                        $random_args = array(
-                                            'category_name' => 'casino, bonuses'
-                                        )
-                                    ?>
-                                    <?php $random = new WP_Query( $random_args ); ?>
-                                    <?php if ( $random->have_posts() ) : ?>
-                                        <div class="entry-content d-flex"> 
-                                            <div class="row pg-empty-placeholder">
-                                                <?php $random_item_number = 0; ?>
-                                                <?php while ( $random->have_posts() && $random_item_number++ < 3 ) : $random->the_post(); ?>
-                                                    <div class="bonus-column col-sm-4 col-4 pl-2 d-flex flex-column col-md-4"> 
-                                                        <figure class="effect-ruby effect-ruby-1">
-                                                            <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="position: relative; overflow: hidden;"> <?php the_post_thumbnail( 'normal' ); ?> </a>
-                                                            <figcaption>
-                                                                <h2 class=""><?php echo get_post_meta( get_the_ID(), 'offer', true ); ?></h2>
-                                                                <div>
-                                                                    <a class="btn btn-light btn-visit btn-sm" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
-                                                                    <a class="btn btn-light btn-sm btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
-                                                                </div>
-                                                            </figcaption>                                                                             
-                                                        </figure>                                                                         
+                                    <div class="mt-5 mb-5 container" id="content" tabindex="-1"> 
+                                        <div class="row"> 
+                                            <div class="col-md-12">
+                                                <h3 class="p-2"><?php _e( 'Usefull information', 'october' ); ?></h3> 
+                                            </div>
+                                        </div>                                                         
+                                        <?php
+                                            $info_args = array(
+                                                'category_name' => 'info'
+                                            )
+                                        ?>
+                                        <?php $info = new WP_Query( $info_args ); ?>
+                                        <?php if ( $info->have_posts() ) : ?>
+                                            <div <?php post_class( 'row' ); ?> id="post-<?php the_ID(); ?>">
+                                                <?php $info_item_number = 0; ?>
+                                                <?php while ( $info->have_posts() && $info_item_number++ < 4 ) : $info->the_post(); ?>
+                                                    <div class="col-md-6 mb-2 col-lg-12 container d-flex">
+                                                        <div class="col-sm-6 col-6 col-md-4">
+                                                            <figure class="snip0077 blue">
+                                                                <?php
+                                                                    if ( has_post_thumbnail() ) {
+                                                                        the_post_thumbnail( 'normal', array(
+                                                                        'class' => 'snip0077'
+                                                                    ) );
+                                                                    }
+                                                                 ?>
+                                                                <a href="<?php echo esc_url( get_permalink() ); ?>">
+                                                                    <figcaption class="snip0077">
+                                                                        <p><?php _e( 'Read more', 'october' ); ?></p>
+                                                                    </figcaption>
+                                                                </a>
+                                                            </figure>
+                                                        </div>
+                                                        <div class="px-3 col-sm-6 col-6 col-md-8">
+                                                            <div class="card-block px-3 ">
+                                                                <a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
+                                                                <p><?php the_content(); ?></p>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 <?php endwhile; ?>
                                                 <?php wp_reset_postdata(); ?>
-                                            </div>                                                             
-                                        </div>
-                                    <?php else : ?>
-                                        <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
-                                    <?php endif; ?> 
+                                            </div>
+                                        <?php else : ?>
+                                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
+                                        <?php endif; ?>
+                                    </div>                                                     
                                 </article>
                             <?php endwhile; ?>
                         <?php else : ?>
