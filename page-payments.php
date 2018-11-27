fd38f614db838dec930c50edde9d8a56e46a681d
MB <wendigotech@gmail.com>
2018-11-27 18:15:47 +0100

    payments page  -sidebar

--- page-payments.php
+++ page-payments.php
@@ -2,9 +2,9 @@
 get_header(); ?>
 
 <div id="page-wrapper" class="wrapper mt-5 pt-5 mb-5 pb-5 d-flex"> 
-    <div class="container ml-5">
+    <div class="container ml-5 mr-5">
         <div class="row">
-            <div class="content-area col-12 ml-0 ml-sm-0 col-lg-12 col-md-12" id="primary" style="background-color: rgba(226, 226, 235, 0.77); border-radius: 3px;"> 
+            <div class="content-area col-12 ml-0 ml-sm-0 col-lg-12 col-md-12 " id="primary" style="background-color: rgba(226, 226, 235, 0.77); border-radius: 3px;"> 
                 <main class="site-main" id="main"> 
                     <div> 
                         <?php if ( have_posts() ) : ?>
@@ -71,79 +71,6 @@ get_header(); ?>
             </div>
         </div>
     </div>                     
-    <div role="complementary" class="col-sm-12 col-lg-3 mr-5 d-none col-8 col-md-4 d-md-inline mr-md-0"> 
-        <div class="sidebar">
-            <row>
-                <?php
-                    $best_offer_args = array(
-                        'tag' => 'best-offer',
-                        'order' => 'DESC'
-                    )
-                ?>
-                <?php $best_offer = new WP_Query( $best_offer_args ); ?>
-                <?php if ( $best_offer->have_posts() ) : ?>
-                    <?php $best_offer_item_number = 0; ?>
-                    <?php while ( $best_offer->have_posts() && $best_offer_item_number++ < 1 ) : $best_offer->the_post(); ?>
-                        <figure style="height: 8cm;" <?php post_class( 'effect-ruby effect-ruby-1 ' ); ?> id="post-<?php the_ID(); ?>">
-                            <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"> <?php the_post_thumbnail( 'large' ); ?> </a>
-                            <figcaption>
-                                <h2><?php echo get_post_meta( get_the_ID(), 'offer', true ); ?></h2>
-                                <div>
-                                    <a class="btn btn-light btn-visit" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
-                                    <a class="btn btn-light btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
-                                </div>
-                            </figcaption>                                             
-                        </figure>
-                    <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>
-                <?php else : ?>
-                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
-                <?php endif; ?>
-            </row>
-            <div class="row">
-                <div class="col-md-12 col-sm-10 col-12 ">
-                    <div class="row pg-empty-placeholder">
-                        <div class="col-md-5"> 
-                            <h4 class="text-center"><?php _e( 'Information for you', 'october' ); ?></h4>
-                            <?php if ( is_active_sidebar( 'side-links' ) ) : ?>
-                                <ul class="list-unstyled text-center">
-                                    <?php dynamic_sidebar( 'side-links' ); ?>
-                                </ul>
-                            <?php endif; ?>
-                        </div>
-                        <?php
-                            $best_offer_args = array(
-                                'tag' => 'best-offer',
-                                'order' => 'DESC'
-                            )
-                        ?>
-                        <?php $best_offer = new WP_Query( $best_offer_args ); ?>
-                        <?php if ( $best_offer->have_posts() ) : ?>
-                            <div <?php post_class( 'col-10 col-md-11 col-lg-7' ); ?> id="post-<?php the_ID(); ?>">
-                                <h4><?php _e( 'Best offer', 'october' ); ?></h4> 
-                                <?php $best_offer_item_number = 0; ?>
-                                <?php while ( $best_offer->have_posts() && $best_offer_item_number++ < 3 ) : $best_offer->the_post(); ?>
-                                    <figure class="effect-ruby effect-ruby-1" style="height: 5cm;">
-                                        <a href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>" style="overflow: hidden;"> <?php the_post_thumbnail( 'normal' ); ?> </a> 
-                                        <figcaption>
-                                            <h3><?php echo get_post_meta( get_the_ID(), 'offer', true ); ?></h3>
-                                            <div>
-                                                <a class="btn btn-light btn-visit btn-sm" href="<?php echo get_post_meta( get_the_ID(), 'link1', true ); ?>"><?php _e( 'Visit Casino', 'october' ); ?></a>
-                                                <a class="btn btn-light btn-sm btn-terms" href="<?php echo get_post_meta( get_the_ID(), 'link2', true ); ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo get_post_meta( get_the_ID(), 't&c', true ); ?>"><?php _e( 'T&amp;C\'s Apply', 'october' ); ?></a>
-                                            </div>
-                                        </figcaption>
-                                    </figure>
-                                <?php endwhile; ?>
-                                <?php wp_reset_postdata(); ?>
-                            </div>
-                        <?php else : ?>
-                            <p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p>
-                        <?php endif; ?>
-                    </div>                                     
-                </div>
-            </div>
-        </div>                         
-    </div>
 </div>                                 
 
 <?php get_footer(); ?>
