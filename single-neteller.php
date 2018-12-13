96de0a8fbbb268506ce94637681e47814a25cd97
MB <wendigotech@gmail.com>
2018-12-14 00:33:51 +0100

    paypal, neteller post templates

--- single-neteller.php
+++ single-neteller.php
@@ -1,7 +1,7 @@
 <?php
 /*
- Template Name: Paypal
- Template Post Type: paypal
+ Template Name: Neteller
+ Template Post Type: neteller
 */
 ?>
 <?php
@@ -23,7 +23,7 @@ get_header(); ?>
                         </article><?php endwhile; ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'october' ); ?></p><?php endif; ?><?php
                             $random_payments_args = array(
                                 'category_name' => 'casino, bonuses',
-                                'tag' => 'paypal',
+                                'tag' => 'netel',
                                 'order' => 'DESC'
                             )
                         ?><?php $random_payments = new WP_Query( $random_payments_args ); ?><?php if ( $random_payments->have_posts() ) : ?><div class="entry-content mt-5"> 
